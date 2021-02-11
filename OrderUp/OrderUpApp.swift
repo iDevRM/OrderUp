b9ab32e145b2acc8115e73f7b9238976cd970566
Rick <rmartinez1611@gmail.com>
2021-02-11 14:00:02 -0500

    Initial Commit

---
+++ OrderUp/OrderUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderUpApp.swift
+//  OrderUp
+//
+//  Created by Ricardo Martinez on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct OrderUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
